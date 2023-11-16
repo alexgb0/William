db8febcac77ff1e318fab8e409c19684c4e5ad45
Alex G. B. <alexgb.git@gmail.com>
2023-11-16 03:19:21 +0100

    now the bot works

---
+++ discord/src/index.ts
@@ -0,0 +1,19 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import config from "../config.json"
+import TelegramBot from "node-telegram-bot-api";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const tg = new TelegramBot(process.env.TG_TOKEN!!);
+
+client.on(Events.MessageCreate, async msg => {
+	if (msg.channelId != config.channel_discord)
+		return;
+	console.log(`Got message ${msg.content}`);
+	const tg_msg = await tg.sendMessage(config.channel_telegram, `${msg.author.displayName}: ${msg.content}`);
+});
+
+client.once(Events.ClientReady, c => {
+	console.log(`Bot on as ${c.user.tag}`);
+})
+
+client.login(process.env.DISCORD_TOKEN);
